e8f01057a64247d42e9e4480986e55ea91d94e60
moyouteam-dz <moyou.team@gmail.com>
2026-09-18 07:16:16 +0200

    feat: implement src/main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,16 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom";
+import { HashRouter } from "react-router-dom";
 import App from "./App";
+import { ReviewSessionProvider } from "./review/ReviewSessionContext";
 import "./styles.css";
 
 createRoot(document.getElementById("root")!).render(
   <StrictMode>
-    <BrowserRouter>
-      <App />
-    </BrowserRouter>
+    <HashRouter>
+      <ReviewSessionProvider>
+        <App />
+      </ReviewSessionProvider>
+    </HashRouter>
   </StrictMode>,
 );
